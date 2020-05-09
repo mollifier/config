4b5278c11e5a753e29b4a3a78d8fb41d3cf6297f
mollifier <mollifier@gmail.com>
2020-05-09 12:18:27 +0900

    Add alias.

--- config.fish
+++ config.fish
@@ -1,1 +1,67 @@
 # config.fish
+
+# aliases #{{{1
+
+#list
+alias ls='ls -F --color=auto'
+alias l='ls'
+alias la='ls -a'
+alias ll='ls -lh'
+alias ld='ls -d'
+alias l1='ls -1'
+alias lt='tree -F'
+
+#file operation
+alias rm='rm -i'
+alias cp='cp -i'
+alias mv='mv -i'
+
+alias mkdir='mkdir -p'
+
+#cd
+alias c='cd'
+alias c-='cd -'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+alias ......='cd ../../../../..'
+
+#vi
+alias vi='vim'
+alias v='vim'
+alias vir='vim -R'
+alias vr='vim -R'
+alias winvi='vim -c "edit ++fileformat=dos ++enc=cp932"'
+alias eucvi='vim -c "edit ++enc=euc-jp"'
+alias gm='gvim'
+
+#grep
+alias gf='grep --with-filename --line-number'
+alias gr='grep --with-filename --line-number --recursive --exclude-dir=.svn'
+
+# show all histories
+alias his='history'
+# show recent 10 histories
+alias h='history --max=10 | cat'
+
+#etc
+alias g='git'
+alias dirs='dirs -p'
+alias ln='ln -s'
+alias jb='jobs -l'
+alias sc=screen
+alias m='man'
+alias eman="LANG=C man"
+alias em="LANG=C man"
+alias di='diff -u'
+alias rlocate='locate --regex'
+alias ema='emacs -nw'
+alias mkzip='zip -q -r'
+alias pc=pwd-clip
+alias sc=scala
+alias scc=scalac
+alias csc=scalac
+
+alias be='bundle exec'
+
