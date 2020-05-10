ad1ab81c2be941a1e38ed29426e8ed3ead7ee68b
mollifier <mollifier@gmail.com>
2020-05-10 11:30:55 +0900

    Add z key binding

--- dot.config/fish/config.fish
+++ dot.config/fish/config.fish
@@ -108,3 +108,21 @@ set -g GHQ_SELECTOR_OPTS "--no-sort --reverse --ansi --color bg+:13,hl:3,pointer
 set -g FZF_LEGACY_KEYBINDINGS 0
 set -g FZF_DEFAULT_OPTS "--height 20 --color bg+:13,hl:3,pointer:7"
 
+# z #{{{1
+# https://github.com/jethrokuan/z
+#
+# require fzf https://github.com/junegunn/fzf
+function __z_select_and_change_directory -d 'Select z directory and change directory'
+    set -l selector fzf
+    set -l selector_options '--no-sort --reverse --ansi --color bg+:13,hl:3,pointer:7'
+
+    if not type -qf $selector
+        echo "ERROR: '$selector' not found." 1>&2
+        return 1
+    end
+
+    z -l | eval $selector $selector_options | sed 's/^[^\s][^\s]*\s\s*//' | read -l select
+    [ -n "$select" ]; and cd -- "$select"
+    commandline -f repaint
+end
+
--- dot.config/fish/functions/fish_user_key_bindings.fish
+++ dot.config/fish/functions/fish_user_key_bindings.fish
@@ -1,2 +1,3 @@
 function fish_user_key_bindings
+  bind \cb __z_select_and_change_directory
 end
