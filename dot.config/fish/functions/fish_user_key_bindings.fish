993f01e063e10461a893ea7fdcf6e2be92d8fa3e
mollifier <mollifier@gmail.com>
2020-05-11 12:03:44 +0900

    Add keybinding

--- dot.config/fish/functions/fish_user_key_bindings.fish
+++ dot.config/fish/functions/fish_user_key_bindings.fish
@@ -2,4 +2,8 @@ function fish_user_key_bindings
   # z
   bind \cxb user_z_select_and_change_directory
   bind \cx\cb user_z_select_and_change_directory
+
+  # decors/fish-ghq
+  bind \cxg __ghq_repository_search
+  bind \cx\cg __ghq_repository_search
 end
