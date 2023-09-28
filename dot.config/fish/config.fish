4b1ad2ed9b69baa7d044158a5c085e851189e064
mollifier <mollifier@gmail.com>
2023-09-28 15:15:21 +0900

    Add alias to pretty-print JSON

--- dot.config/fish/config.fish
+++ dot.config/fish/config.fish
@@ -130,6 +130,8 @@ abbr --add T --position anywhere '| tail'
 abbr --add G --position anywhere '| grep'
 abbr --add V --position anywhere '| vim -R -'
 abbr --add C --position anywhere '| copy_to_clipboard'
+abbr --add J --position anywhere '| jq "." | bat --language json'
+
 # fisher packages
 # https://github.com/jorgebucaran/fisher
 #
