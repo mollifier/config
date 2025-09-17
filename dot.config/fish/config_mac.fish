69c43a3456015260a4cb13b50780f33f550ea2f3
mollifier <mollifier@gmail.com>
2025-09-17 14:57:04 +0900

    Add Mac fish settings

--- dot.config/fish/config_mac.fish
+++ dot.config/fish/config_mac.fish
@@ -1,1 +1,9 @@
+if type -q brew
+  brew shellenv fish | source
+end
 
+# deno
+# https://deno.land/
+# for vim plugins (ddc.vim)
+set -x DENO_INSTALL $HOME/.deno
+fish_add_path --path $DENO_INSTALL/bin
